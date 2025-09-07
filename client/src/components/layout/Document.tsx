60ae238f02519cf2654026638071f8948c8933c6
BTang <benjamin.code.tang@gmail.com>
2025-09-07 18:53:40 -0400

    fix naming

--- client/src/components/layout/Document.tsx
+++ client/src/components/layout/Document.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRef, useState } from "react";
-import { Page, View, Document, Text, pdf } from "@react-pdf/renderer";
+import { Page, View, Document as PDFDocument, Text, pdf } from "@react-pdf/renderer";
 import { saveAs } from "file-saver";
 import dynamic from "next/dynamic";
 import { sampleResumes } from "@/data/constants";
@@ -157,7 +157,7 @@ const Doc = ({
   };
 
   const ResumeDocs = ({ data }: PDFProps) => (
-    <Document title={data.resumeName ? `${data.resumeName}.pdf` : "Resume.pdf"}>
+    <PDFDocument title={data.resumeName ? `${data.resumeName}.pdf` : "Resume.pdf"}>
       <Page size="LETTER" style={styles.page}>
         <View style={{ gap: 12 }}>
           <HeaderSection header={data.header} />
@@ -168,11 +168,11 @@ const Doc = ({
           </View>
         </View>
       </Page>
-    </Document>
+    </PDFDocument>
   );
 
   const LetterDocs = ({ data }: PDFProps) => (
-    <Document
+    <PDFDocument
       title={data.letterName ? `${data.letterName}.pdf` : "CoverLetter.pdf"}
     >
       <Page size="LETTER" style={styles.page}>
@@ -189,7 +189,7 @@ const Doc = ({
           <LetterSection name={data.header.name} letter={data.letter} />
         </View>
       </Page>
-    </Document>
+    </PDFDocument>
   );
 
   const saveFile = async () => {
