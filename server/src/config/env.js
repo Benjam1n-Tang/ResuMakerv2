0f101594c4d3f9ce87f8cb35119b0eb2847dd412
BTang <benjamin.code.tang@gmail.com>
2025-09-06 22:42:05 -0400

    fixed vercel deployment for server

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -2,4 +2,9 @@ import { config } from "dotenv";
 
 config({ path: ".env" });
 
-export const { PORT, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
+export const {
+  PORT = process.env.PORT || 8000,
+  DB_URI,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+} = process.env;
